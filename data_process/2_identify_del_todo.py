c832d1f8a92a7457779c523c583d11f98c0cba7f
zpgao <beyond_acm@163.com>
2021-01-02 15:45:33 +0800

    update 2_identify_del_todo.py

--- data_process/2_identify_del_todo.py
+++ data_process/2_identify_del_todo.py
@@ -3,27 +3,43 @@ import re
 import json
 from nltk.tokenize import sent_tokenize
 
+'''
 del_todo_pattern = [ \
-'<nl> - # todo', \
-'<nl> - # # todo', \
-'<nl> - # # # todo', \
-'<nl> - # # # # todo', \
-'<nl> - # # # # # todo', \
-'<nl> - " " " todo', \
-"<nl> - ' ' ' todo", \
+'<nl> - / / todo', \
+'<nl> - / * todo' ,\
 ]
 
 undel_todo_pattern = [ \
-'<nl> # todo', \
-'<nl> # # todo', \
-'<nl> # # # todo', \
-'<nl> # # # # todo', \
-'<nl> # # # # # todo', \
 '<nl> " " " todo', \
 "<nl> ' ' ' todo", \
 ] 
+'''
+
+with open('./todo_diff.out', 'r') as fin, \
+    open('./todo_deleted.out', 'w') as del_todo, \
+    open('./todo_undeleted.out', 'w') as undel_todo:
+    for line in fin:
+        if len(line.split('todo')) > 2:
+            continue
+        # print(line.strip())
+        repo_file, commit_now, commit_parent, \
+        commit_msg_pro, commit_diff_pro = line.strip().split('\t')
+        # print(commit_diff_pro)
+        commit_diff_pro_splits = commit_diff_pro.split("<nl>") 
+        for e in commit_diff_pro_splits:  
+            e = e.strip()
+            # print(e)
+            if e.startswith('-') and "todo" in e:
+                # print("founded:", e)
+                del_todo.write(line)
+
+            if not e.startswith('+'): 
+                if not e.startswith('-'):
+                    if "todo" in e:
+                        undel_todo.write(line)
 
 
+'''
 with open('./todo_diff.out', 'r') as fin, \
     open('./todo_deleted.out', 'w') as del_todo, \
     open('./todo_undeleted.out', 'w') as undel_todo:
@@ -34,5 +50,5 @@ with open('./todo_diff.out', 'r') as fin, \
             del_todo.write(line)
         if any(pattern in line.strip() for pattern in undel_todo_pattern): 
             undel_todo.write(line) 
-
+'''
 

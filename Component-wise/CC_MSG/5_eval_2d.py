e41191d8f639f49d085a0218b466ab47481cb800
zpgao <beyond_acm@163.com>
2021-02-04 22:37:58 +0800

    add src code for Component-wise

---
+++ Component-wise/CC_MSG/5_eval_2d.py
@@ -0,0 +1,111 @@
+from utils import *
+from Bert_MLP import Model, Config
+import torch.nn.functional as F
+import torch.nn
+import numpy
+# from Bert_CNN import Model, Config
+# torch.manual_seed(777)
+
+# Load the iterator
+with open('./data/test_data/test_dataloader.pkl', 'rb') as handler:
+    test_dataloader = pickle.load(handler)
+print("test dataloader loaded!")
+
+PATH = './model_save/step1-1000/model.ckpt'
+config = Config()
+
+# Load Model
+model = Model(config).to(config.device)
+model.load_state_dict(torch.load(PATH))
+model.eval()
+print('Model Loaded!')
+
+# ========================================
+#              Testing 
+# ========================================
+
+# After the completion of each training epoch, measure our performance on
+# our test set. 
+print("")
+print("Running Testing...")
+
+t0 = time.time()
+# Put the model in evaluation mode--the dropout layers behave differently
+# during evaluation.
+
+# Tracking variables
+total_eval_accuracy = 0
+total_eval_loss = 0
+nb_eval_steps = 0
+
+prob_result_lst = []
+
+# Evaluate data for one epoch 
+for batch in test_dataloader:
+    # Unpack this training batch from our dataloader 
+    # As we unpack the batch, we will also copy each tensor to the GPU using 'to' method
+    # 'batch' contains three pytorch tensors:
+    # [0]: input ids
+    # [1]: attention masks
+    # [2]: labels
+    cc_input_ids   = batch[0].to(config.device)
+    cc_input_mask  = batch[1].to(config.device)
+    cc_input_types = batch[2].to(config.device)
+    
+    td_input_ids   = batch[3].to(config.device)
+    td_input_mask  = batch[4].to(config.device)
+    td_input_types = batch[5].to(config.device)
+
+    b_labels = batch[6].to(config.device)
+    
+    with torch.no_grad():
+    # Forward pass, calculate logit predictions.  
+    # token_type_ids is the same as "segment ids",  
+    # which differentiates sentence 1 and 2 in 2-sentence tasks.  
+    # values prior to applying an activation function like the softmax. 
+        cc_input = (cc_input_ids, cc_input_mask, cc_input_types)
+        td_input = (td_input_ids, td_input_mask, td_input_types)
+
+        b_outputs = model(cc_input, td_input)
+
+    loss = F.cross_entropy(b_outputs, b_labels)
+    # Accumulate the test loss.
+    total_eval_loss += loss.item()
+     
+    # move labels to CPU 
+    # preds_raw = b_outputs.data
+    
+    # torch tensor  
+    lr = torch.nn.Softmax(dim=1)
+    preds_prob = lr(b_outputs.data)
+    # print("lr prob:", preds_prob, type(preds_prob), preds_prob.shape)
+    # tensor to numpy  
+    preds_prob = preds_prob.cpu().detach().numpy() 
+    # print("lr prob:", preds_prob, type(preds_prob), preds_prob.shape)
+    prob_result_lst.append( preds_prob ) 
+
+    
+    preds = torch.max(b_outputs.data, 1)[1].cpu().numpy()
+    labels = b_labels.to('cpu').numpy()
+    # print("preds:", type(preds), preds.shape)
+    # print("preds:", preds)
+    # print("labels:", type(labels), labels.shape)
+    # print("labels:", labels)
+
+    # Calculate the accuracy for this batch of test sentences, and
+    total_eval_accuracy += flat_accuracy(preds, labels)
+    # print("total eval acc:", total_eval_accuracy)
+    # break
+
+prob_result = numpy.vstack( prob_result_lst )
+print("prob_result:", type(prob_result), prob_result.shape )
+
+# output to csv   
+numpy.savetxt("./data/test_data/test_prob.csv", prob_result, delimiter=",")
+
+# Report the final accuracy for this testing run.
+print(total_eval_accuracy)
+print(len(test_dataloader))
+avg_val_accuracy = total_eval_accuracy / len(test_dataloader)
+print("  Accuracy: {0:.4f}".format(avg_val_accuracy))
+
---
+++ Component-wise/TD_CC/6_sum_result.py
@@ -0,0 +1,91 @@
+import pickle
+
+def make_test_result_file():
+    '''
+    '''    
+    test_result = []
+    with open('./data/test_data/cc_todo_pairs.test', 'r') as test_data, \
+        open('./data/test_data/test_prob.csv', 'r') as test_prob: 
+
+        for data_line, result_line in zip(test_data, test_prob):
+            data_line_split = data_line.strip().split('\t')
+            code_change = data_line_split[0]
+            todo_comment = data_line_split[1]
+            commit_msg = data_line_split[2]
+            label = data_line_split[3]
+            info = data_line_split[4:]
+
+            label = int(label)
+            neg_prob, pos_prob = result_line.strip().split(',') 
+            neg_prob = float(neg_prob) 
+            pos_prob = float(pos_prob) 
+            
+            if neg_prob > pos_prob:
+                pred_label = 0     
+            else:
+                pred_label = 1
+
+            # print(neg_prob, pos_prob, label)
+            test_result.append( (code_change, todo_comment, commit_msg, label, \
+                                 pred_label, neg_prob, pos_prob, \
+                                 info ) )
+            # break
+    return test_result
+    pass
+    
+def make_train_result_file():
+    '''
+    '''    
+    train_result = []
+    with open('./data/train_data/cc_todo_pairs.train', 'r') as train_data, \
+        open('./data/train_data/train_prob.csv', 'r') as train_prob: 
+
+        for data_line, result_line in zip(train_data, train_prob):
+            data_line_split = data_line.strip().split('\t')
+            code_change = data_line_split[0]
+            todo_comment = data_line_split[1]
+            commit_msg = data_line_split[2]
+            label = data_line_split[3]
+            info = data_line_split[4:]
+
+            label = int(label)
+            
+            neg_prob, pos_prob = result_line.strip().split(',') 
+            neg_prob = float(neg_prob) 
+            pos_prob = float(pos_prob) 
+            
+            if neg_prob > pos_prob:
+                pred_label = 0     
+            else:
+                pred_label = 1
+
+            # print(neg_prob, pos_prob, label)
+            train_result.append( (code_change, todo_comment, commit_msg, label, \
+                                  pred_label, neg_prob, pos_prob, \
+                                  info ) )
+            # break
+    return train_result
+    pass
+ 
+test_result = make_test_result_file()
+print("test_result:", type(test_result), len(test_result))
+with open('./result/test_result/test_result.pkl', 'wb') as handler:
+    pickle.dump(test_result, handler)   
+
+with open('./result/test_result/test.result', 'w') as fout:
+    for e in test_result: 
+        code_change = e[0]
+        todo_comment = e[1]
+        commit_msg = e[2]
+        true_label = e[3]
+        pred_label = e[4]
+        neg_prob = e[5]
+        pos_prob = e[6]
+        info = e[7]
+        fout.write( str(true_label) + '\t' + str(pred_label) )
+        fout.write( '\n')
+        
+# train_result = make_train_result_file()
+# print("train_result:", type(train_result), len(train_result))
+# with open('./result/train_result/train_result.pkl', 'wb') as handler:
+#     pickle.dump(train_result, handler)
